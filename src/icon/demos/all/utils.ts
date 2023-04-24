83bc193d3e5e766ba94e1fbab36c320183d6e93e
caijf <369756941@qq.com>
2023-04-24 13:25:51 +0800

    docs: 更新下载svg方法

--- src/icon/demos/all/utils.ts
+++ src/icon/demos/all/utils.ts
@@ -1,33 +1,4 @@
-const ajax = (
-  url: string,
-  callback: (res: any) => void,
-  responseType?: XMLHttpRequestResponseType,
-) => {
-  const xhr = new XMLHttpRequest();
-  xhr.open('get', url, true);
-  if (responseType) {
-    xhr.responseType = responseType;
-  }
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      callback(xhr.response);
-    }
-  };
-  xhr.send();
-};
-
-export const download = (url: string, fileName: string) => {
-  const a = document.createElement('a');
-  a.setAttribute('href', url);
-  a.setAttribute('download', fileName);
-  a.click();
-};
-
-export const downloadBlob = (blob: Blob, fileName: string) => {
-  const blobUrl = URL.createObjectURL(blob);
-  download(blobUrl, fileName);
-  URL.revokeObjectURL(blobUrl);
-};
+import { download, fileReader } from 'util-helpers';
 
 export const processSvg = (svgStr: string, fontSize: number | string, color: string) => {
   const size = typeof fontSize === 'number' ? `${fontSize}px` : fontSize;
@@ -41,23 +12,15 @@ export const downloadSvg = (
   fileName: string,
   { fontSize, color }: { fontSize: number | string; color: string },
 ) => {
-  ajax(
-    url,
-    (res) => {
-      // console.log(res);
-      const fr = new FileReader();
-      fr.onload = () => {
-        // console.log(fr.result);
-        const ret = processSvg(fr.result as string, fontSize, color);
-        // console.log(ret);
-        const blob = new Blob([ret], { type: res.type });
-        // console.log(blob);
-        downloadBlob(blob, fileName);
-      };
-      fr.readAsText(res);
+  download(url, {
+    dataType: 'url',
+    fileName,
+    transformResponse: async (res) => {
+      const text = await fileReader(res, 'text');
+      const ret = processSvg(text, fontSize, color);
+      return new Blob([ret], { type: res.type });
     },
-    'blob',
-  );
+  });
 };
 
 export const formatPx = (num: number | string) => {
