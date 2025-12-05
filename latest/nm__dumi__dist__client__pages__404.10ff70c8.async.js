96f6f300d907d3f130d690a04daedac9fc1b54b1
caijf <caijf@users.noreply.github.com>
2025-12-05 01:16:02 +0000

    deploy: b1d853828a744efb114b2f4462a13aef71b59cb8

---
+++ latest/nm__dumi__dist__client__pages__404.10ff70c8.async.js
@@ -0,0 +1,29 @@
+"use strict";(self.webpackChunkdoly_icons=self.webpackChunkdoly_icons||[]).push([[543],{32025:function(u,a,t){t.r(a),t.d(a,{default:function(){return S}});var o=t(85897),e=t(75271),l=t(20058),r=t.n(l),s=t(40488),m=t.n(s),c=t(33131),v=t.n(c),_=t(8706),E=t.n(_),h=t(35489),y=t.n(h),P=t(4834),g=t.n(P),d=t(26494),n={};n.styleTagTransform=g(),n.setAttributes=E(),n.insert=v().bind(null,"head"),n.domAPI=m(),n.insertStyleElement=y();var T=r()(d.Z,n),M=d.Z&&d.Z.locals?d.Z.locals:void 0,A=function(){var f=(0,o.YB)(),i=(0,o.bU)();return e.createElement("div",{className:"dumi-default-not-found"},e.createElement("h1",null,f.formatMessage({id:"404.title"})),e.createElement(o.rU,{to:"base"in i?i.base:"/",replace:!0},f.formatMessage({id:"404.back"})," \u2192"))},S=A},26494:function(u,a,t){var o=t(72865),e=t.n(o),l=t(31510),r=t.n(l),s=r()(e());s.push([u.id,`.dumi-default-not-found {
+  display: flex;
+  flex-direction: column;
+  height: 65vh;
+  align-items: center;
+  justify-content: center;
+}
+.dumi-default-not-found > h1 {
+  position: relative;
+  margin: 24px 0;
+  color: #e4e9ec;
+  font-size: 78px;
+  font-weight: 700;
+  text-shadow: -1px -1px 0 #d0d5d8;
+}
+[data-prefers-color="dark"] .dumi-default-not-found > h1 {
+  color: #2a353c;
+  text-shadow: -1px -1px 0 #1c2022;
+}
+.dumi-default-not-found > a {
+  color: #1677ff;
+}
+[data-prefers-color="dark"] .dumi-default-not-found > a {
+  color: #0053c8;
+}
+.dumi-default-not-found > a:not(:hover) {
+  text-decoration: none;
+}
+`,""]),a.Z=s}}]);
